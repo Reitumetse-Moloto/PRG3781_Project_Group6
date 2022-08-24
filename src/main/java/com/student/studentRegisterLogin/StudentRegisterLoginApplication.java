afc733e69d2b78345759460d47addb37b32c71c4
Wouter <woutermalan7909@gmail.com>
2022-08-24 17:47:08 +0200

    added spring boot files

---
+++ src/main/java/com/student/studentRegisterLogin/StudentRegisterLoginApplication.java
@@ -0,0 +1,13 @@
+package com.student.studentRegisterLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegisterLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegisterLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/studentRegisterLogin/StudentRegisterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.studentRegisterLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegisterLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
